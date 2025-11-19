a88f38aeea44079170eb9bdd5068338bfc9d394d
Dedede12345 <gleb@adenisov.com>
2025-11-19 16:12:36 +0100

    Added TestPersonCorrectAge testcase

--- BYT_04_Test/PersonTest.cs
+++ BYT_04_Test/PersonTest.cs
@@ -12,4 +12,15 @@ public class Tests
         var address = new Address("some", "another", "dom", "somecode", "someplace");
         Assert.Throws<ArgumentException>(() => new Person("Gleb", null, "Denisov", invalidDate, "male", "+48999999999", "email@gmail.com", address));
     }
+    
+    [Test]
+    public void TestPersonCorrectAge()
+    {
+        DateTime date1 = new DateTime(2015, 8, 28);
+
+        var address = new Address("some", "another", "dom", "somecode", "someplace");
+        var person = new Person("Gleb", null, "Denisov", date1, "male", "+48999999999", "email@gmail.com", address);
+        
+        Assert.That(person.Age, Is.EqualTo(10));
+    }
 }
