a0ad48091e59a3c64dc83023a7384aef1cd33a5b
Dedede12345 <gleb@adenisov.com>
2025-11-19 17:04:08 +0100

    Changed directory structure

---
+++ BYT_04/HumanResources/Customer.cs
@@ -0,0 +1,17 @@
+namespace BYT_04;
+
+public class Customer : Person
+{
+    public bool IsVip { get; set; }
+    public int LoyaltyPoints { get; set; }
+
+    public Customer()
+    {
+    }
+
+    public Customer(string name, string? middleName, string surname, DateTime birthDate, string gender, string phoneNumber, string email, Address address, bool isVip, int loyaltyPoints) : base(name, middleName, surname, birthDate, gender, phoneNumber, email, address)
+    {
+        IsVip = isVip;
+        LoyaltyPoints = loyaltyPoints;
+    }
+}
