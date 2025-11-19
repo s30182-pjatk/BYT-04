fe98d1b0539337f5df18404833707d74f37c4e2f
Dedede12345 <gleb@adenisov.com>
2025-11-19 15:58:56 +0100

    Added Person class and PersonTest

---
+++ BYT_04/Person.cs
@@ -0,0 +1,64 @@
+﻿namespace BYT_04;
+
+public class Person
+{
+    public readonly string Name;
+    public readonly string? MiddleName;
+    public readonly string Surname;
+    public readonly DateTime BirthDate;
+    public readonly string Gender;
+    public readonly int Age;
+    public readonly string PhoneNumber;
+    public readonly string Email;
+
+    public Person(string name, string? middleName, string surname, DateTime birthDate, string gender, string phoneNumber, string email)
+    {
+        
+        if (birthDate > DateTime.Today)
+        {
+            throw new ArgumentException("Birth date cannot be in the future.");
+        }
+
+        Name = name;
+        MiddleName = middleName;
+        Surname = surname;
+        BirthDate = birthDate;
+        Gender = gender;
+        PhoneNumber = phoneNumber;
+        Email = email;
+
+        Age = CalculateAge(birthDate);
+    }
+
+    private int CalculateAge(DateTime birthDate)
+    {
+        var today = DateTime.Today;
+        int age = today.Year - birthDate.Year;
+
+        if (birthDate.Date > today.AddYears(-age))
+        {
+            age--;
+        }
+
+        return age;
+    }
+    
+}
+
+public class Address
+{
+    public readonly string Street;
+    public readonly string City;
+    public readonly string State;
+    public readonly string PostalCode;
+    public readonly string Country;
+
+    public Address(string street, string city, string state, string postalCode, string country)
+    {
+        Street = street;
+        City = city;
+        State = state;
+        PostalCode = postalCode;
+        Country = country;
+    }
+}
---
+++ BYT_04_Test/PersonTest.cs
@@ -0,0 +1,13 @@
+﻿using BYT_04;
+
+namespace BYT_04_Test;
+
+public class Tests
+{
+    [Test]
+    public void TestPersonInvalidDate()
+    {
+        var invalidDate = DateTime.Today.AddDays(1);
+        Assert.Throws<ArgumentException>(() => new Person("Gleb", null, "Denisov", invalidDate, "male", "+48999999999", "email@gmail.com"));
+    }
+}
