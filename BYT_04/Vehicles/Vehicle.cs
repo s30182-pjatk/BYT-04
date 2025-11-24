74e78653970af1188434c40d3a22a650461bf48c
s31798 <s31798@pjwstk.edu.pl>
2025-11-24 21:31:12 +0100

    Stearted Vehicle class

--- BYT_04/Vehicles/Vehicle.cs
+++ BYT_04/Vehicles/Vehicle.cs
@@ -1,6 +1,17 @@
 namespace BYT_04.Vehicles;
 
+[Serializable]
 public class Vehicle
 {
+    private string _plateNumber;
+    private string _model;
+    private int _capacity;
+    private bool _containMedKit;
+
+    public string PlateNumber
+    {
+        get => _plateNumber;
+       
+    }
     
 }
