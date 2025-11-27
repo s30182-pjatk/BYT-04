1acba8f57f82ee3019c4751c66834340246d3032
s31798 <s31798@pjwstk.edu.pl>
2025-11-27 15:23:05 +0100

    Began REservation vehicle

--- BYT_04/Reservations/ReservationVehicle.cs
+++ BYT_04/Reservations/ReservationVehicle.cs
@@ -1,6 +1,39 @@
 namespace BYT_04.Reservations;
 
+using BYT_04.Utility;
+using BYT_04.Vehicles;
+
 public class ReservationVehicle
 {
+    private Vehicle _vehicle = null!;
+    public Vehicle Vehicle
+    {
+        get => _vehicle;
+        set => _vehicle = value ?? throw new ArgumentException("Vehicle cannot be null.");
+    }
+    private Reservation _reservation = null!;
+    public Reservation Reservation
+    {
+        get => _reservation;
+        set => _reservation = value ?? throw new ArgumentException("Reservation cannot be null.");
+    }
+    private string _usgagePurpose;
+    public string UsgagePurpose
+    {
+        get => _usgagePurpose;
+        set => _usgagePurpose = value.ValidateRequiredString(nameof(UsgagePurpose));
+    }
+    private string _conditionBefore;
+    public string ConditionBefore
+    {
+        get => _conditionBefore;
+        set => _conditionBefore = value.ValidateRequiredString(nameof(ConditionBefore));
+    }
+    private string? _conditionAfter;
+    public string? ConditionAfter
+    {
+        get => _conditionAfter;
+        set => _conditionAfter = string.IsNullOrWhiteSpace(value) ? null : value;
+    }
     
 }
